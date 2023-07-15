b6579ec6d44b97a44dd0a7a16a8f8748372c2c5d
Anthony Vettraino <apvett@umich.edu>
2023-07-15 17:37:42 -0400

    db issue fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const db = knex({
 const app = express();
 // app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("it's working!");
